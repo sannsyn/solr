8d9674325abe8d554919aae9bc9f0888a9e25b1c
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-03-22 13:25:29 +0300

    Init

---
+++ library/Solr/Prelude.hs
@@ -0,0 +1,18 @@
+module Solr.Prelude
+( 
+  module Exports,
+)
+where
+
+
+-- base-prelude
+-------------------------
+import BasePrelude as Exports
+
+-- bytestring
+-------------------------
+import Data.ByteString as Exports (ByteString)
+
+-- text
+-------------------------
+import Data.Text as Exports (Text)
